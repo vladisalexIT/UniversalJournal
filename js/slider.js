a80fbebfde664e74041a966eae3889e857f332e6
vladisalex-it <vl4disalex@gmail.com>
2025-12-09 17:39:55 +0300

    feat: Implement final highlights section and attach jQuery slider to it

---
+++ js/slider.js
@@ -0,0 +1,10 @@
+$(function(){
+  $('.single-item').slick({
+    dots: true,
+    arrows: false,
+    autoplay: true,  
+    autoplaySpeed: 3000,
+    pauseOnHover: true, 
+    pauseOnFocus: true
+  });
+});
